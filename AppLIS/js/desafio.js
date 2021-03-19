501ac7c51a8cc1f8e04aa29c91a63e5a5753f5bc
Thiágo Morais <35998529+moraisthiago@users.noreply.github.com>
2021-03-19 00:33:48 -0300

    Obtendo sobrenome - Forma 2

--- AppLIS/js/desafio.js
+++ AppLIS/js/desafio.js
@@ -1,4 +1,4 @@
-let nome = " Severino da Silva Cavalcante ";
+let nome = " Severino da Silva Cavalcante de Oliveira ";
 console.log(nome.length);
 nome = nome.trim();
 console.log(nome.length);
@@ -6,7 +6,17 @@ console.log(nome);
 
 let divisaoNome = nome.split(" ");
 console.log(divisaoNome);
+
 let primeiroNome = divisaoNome[0];
 console.log(primeiroNome);
+
+// Sobrenome - Forma 1
 let sobrenome = nome.slice(primeiroNome.length+1, nome.length);
+console.log(sobrenome);
+
+// Sobrenome - Forma 2
+sobrenome = ""
+for (let i = 1; i < divisaoNome.length; i++){
+  sobrenome += divisaoNome[i] + " "
+}
 console.log(sobrenome);
