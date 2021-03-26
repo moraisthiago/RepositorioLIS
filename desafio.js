87d7157b2803240880811cb010b350ada75d41c1
Thiágo Morais <35998529+moraisthiago@users.noreply.github.com>
2021-03-26 17:37:37 -0300

    Desafio do sobrenome

---
+++ desafio.js
@@ -0,0 +1,23 @@
+let nome = " Severino da Silva Cavalcante de Oliveira ";
+console.log(nome.length);
+nome = nome.trim();
+console.log(nome.length);
+console.log(nome);
+
+let divisaoNome = nome.split(" ");
+console.log(divisaoNome);
+
+let primeiroNome = divisaoNome[0];
+console.log(primeiroNome);
+
+// Sobrenome - Forma 1
+let sobrenome = nome.slice(primeiroNome.length+1, nome.length);
+console.log(sobrenome);
+
+// Sobrenome - Forma 2
+sobrenome = ""
+for (let i = 1; i < divisaoNome.length; i++){
+  sobrenome += divisaoNome[i] + " "
+}
+sobrenome = sobrenome.trim()
+console.log(sobrenome);
